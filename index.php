5915023e7f1910f81c3a81414786b5789823769b
emil022n <emil022n@edu.campusvejle.dk>
2017-06-15 11:24:48 +0200

    tilføjede header med nav og banner

--- index.php
+++ index.php
@@ -2,17 +2,40 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div id="wrapper">
     <header>
-      
+      <div id="headerCont">
+        <div id="headerIcons">
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <i class="fa fa-youtube" aria-hidden="true"></i>
+        </div>
+        <div id="headerCart">
+          <a href="#">Min konto</a>
+          <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+          <a href="#">Min kurv</a>
+        </div>
+      </div>
+      <img src="img/banner.jpg" alt="Organic Surge hudcreme">
+      <nav>
+        <a href="#">Forside</a>
+        <a href="#">Shop</a>
+        <a href="#">Garanti</a>
+        <a href="#">Om os</a>
+        <a href="#">Kontakt</a>
+        <a href="#">Log ind</a>
+      </nav>
     </header>
     <aside></aside>
     <main></main>
     <footer></footer>
   </div>
+  <script src="https://use.fontawesome.com/d333651ce6.js"></script> 
 </body>
 </html>
